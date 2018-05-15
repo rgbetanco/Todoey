81534465236bbf965cc4717abadee91d9f458efa
Ronald <rgbetanco@gmail.com>
2018-05-15 16:05:29 +0800

    Add alert with text field completed

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray  = ["Find Mike", "Bug Eggs", "Destroy Demogorgon"];
+    var itemArray  = ["Find Mike", "Bug Eggs", "Destroy Demogorgon"];
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +39,25 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
-
+    
+    //MARK - Add new Items
+    @IBAction func addButtonPress(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the add item button on our uialert
+            if textField.text != nil {
+                self.itemArray.append(textField.text!)
+            }
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new Item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
